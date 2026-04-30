689f9919af61f9c7fe2acfdb1cab52fd6e8305df
nibin-matteo-git <nibinmatteo5@gmail.com>
2026-04-30 13:12:47 +0100

    Clean up main.go

--- main.go
+++ main.go
@@ -8,11 +8,6 @@ import (
 )
 
 func main(){
-	// myList := todo.NewList()
-	// myList.AddTodo("trial", "", time.Now())
-	// fmt.Println(myList.ToString())
-	// myList.CompleteTodo(0)
-	// fmt.Println(myList.ToString())
 	todo.InitDB()
 	log.Println("initdb done successfully")
 	todo.GetTodos()
@@ -20,28 +15,3 @@ func main(){
 }
 
 var DB *sql.DB
-
-// func initDB(){
-// 	var err error
-// 	DB, err = sql.Open("sqlite3", "./todos.db")
-// 	if err != nil{
-// 		log.Println(err)
-// 	}
-// 	// sqlStmt := `
-// 	// CREATE TABLE IF NOT EXISTS todos (
-// 	// id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-// 	// title TEXT
-// 	// );`
-// 	sqlStmt := 	`select * from todos;`
-// 	// _, err := DB.Exec(sqlStmt)
-// 	// if err != nil {
-// 	// 	  log.Fatal("Error creating table: %q: %s\n", err, sqlStmt) 
-// 	// }
-
-// 	rows, err := DB.Query(sqlStmt)
-// 	if err != nil {
-// 		  log.Fatal("Error creating table: %q: %s\n", err, sqlStmt) 
-// 	}
-// 	defer rows.Close()
-
-// }
